b42c234726ba87d834caddcefbeda7969ca99b28
rodrigo-loayza <loayza.rs@pucp.edu.pe>
2024-07-05 06:14:45 -0500

    U20180359 - Host fix

--- UrinApp/server.py
+++ UrinApp/server.py
@@ -62,4 +62,4 @@ def send_message(payload: Payload):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
